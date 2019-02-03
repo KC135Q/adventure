8a2a50a3e85653bcaad87242beb3cfef44db16e1
Dan K <KC135Q@users.noreply.github.com>
2019-02-03 14:04:27 -0500

    JSON movement working. Next up is an inventory modal

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -4,6 +4,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle'
 import Work from '@material-ui/icons/Work'
 import PhoneInTalk from '@material-ui/icons/PhoneInTalk'
 import Spa from '@material-ui/icons/Spa'
+const Map = require ('./map.json')
 
 const bgImage = "./meadow_blue.png"
 
@@ -60,57 +61,127 @@ class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: 'Welcome!'
-    };
+      message: 'Welcome!',
+      location: Map.Home
+    }
+  }
+
+  updateLocation = (name) => {
+    this.setState({
+      location: Map[name],
+      message: Map[name].message
+    }, () =>{
+      console.log(`Location ${[this.state.location.name]}`)
+    })
+  }
+
+  componentDidMount() {
+    this.setState({
+      location: Map.Home,
+      message: Map.Home.message
+    }, () =>{
+      console.log(`North? ${this.state.location.move.North}`)
+    })
   }
 
   render() {
 
   return(
-    <Grid container spacing={8} style={ bgStyle }>
+    <Grid container spacing={8} style={bgStyle}>
       <Grid container item xs={12} spacing={24}>
         <Grid item xs={2}>
           <AccountCircle 
             style={iconStyle} 
-            onMouseOver={() => {this.setState({message: 'view profile'})}}
-            onMouseLeave={() => {this.setState({message: 'Remember to sleep when you can'})}}
+            onMouseOver={() => {this.setState({message: 'Profile'})}}
+            onMouseLeave={() => {this.setState({message: this.state.location.message})}}
           />
         </Grid>
         <Grid item xs={8} style={northStyle}>
-          <Button color="primary" variant="contained" >Go North</Button>
+          {(this.state.location.move.North !== null) ?
+            <Button 
+              color="primary" 
+              variant="contained" 
+              onClick={() => {this.updateLocation(this.state.location.move.North)}}
+              >Go North</Button> :
+            <p> </p>
+          }
         </Grid>
         <Grid item xs={2} >
-          <Work style={iconStyle} onMouseOver={() => {this.setState({message: 'check inventory'})}}/>
+          <Work
+            style={iconStyle}
+            onMouseOver={() => {
+              this.setState({message: 'Inventory'})
+            }}
+            onMouseLeave={() => {
+              this.setState({message: this.state.location.message})
+            }}
+          />
         </Grid>
       </Grid>
 
       <Grid container item xs={12} spacing={24} style={vAlignStyle}  >
         <Grid item xs={3} style={westStyle}>
-          <Button color="primary" variant="contained">Go West</Button>
+          {(this.state.location.move.West !== null) ?
+            <Button 
+              color="primary" 
+              variant="contained" 
+              onClick={() => {this.updateLocation(this.state.location.move.West)}}
+            >Go West</Button> :
+            <p> </p>
+          }
         </Grid>
         <Grid item xs={6} >
-          < img src="./cabin_768x450.jpg" alt="Placeholder"  />
+          < img src={`./${this.state.location.img}`} alt="Placeholder" />
+          <p style={{ backgroundColor: 'rgba(0,0,0,.25', color: 'rgba(255,255,255,0.75)'}}>{this.state.message}</p>
         </Grid>
         <Grid item xs={3} style={eastStyle}>
-          <Button color="primary" variant="contained">Go East</Button> 
+          {(this.state.location.move.East !== null) ?
+            <Button 
+              color="primary" 
+              variant="contained" 
+              onClick={() => {this.updateLocation(this.state.location.move.East)}}
+            >Go East</Button> :
+            <p> </p>
+          }
         </Grid>
       </Grid>
 
       <Grid container item xs={12} spacing={24}>
         <Grid item xs={2}>
-          <Spa style={iconStyle} onMouseOver={() => {this.setState({message: 'get objects'})}}/>
+          <Spa
+            style={iconStyle}
+            onMouseOver={() => {
+              this.setState({message: 'Objects'})
+            }}
+            onMouseLeave={() => {
+              this.setState({message: this.state.location.message})
+            }}
+          />
         </Grid>
-        <Grid item xs={8}>
-          <Button color="primary" variant="contained" style={southStyle}>Go South</Button>
-          <p style={{ backgroundColor: 'rgba(0,0,0,.25', color: 'rgba(255,255,255,0.75)'}}>{this.state.message}</p>
+        <Grid item xs={8} style={southStyle}>
+          {(this.state.location.move.South !== null) ?
+            <Button 
+              color="primary" 
+              variant="contained" 
+              onClick={() => {this.updateLocation(this.state.location.move.South)}}
+            >Go South</Button> :
+            <p> </p>
+          }
         </Grid>
         <Grid item xs={2}>
-          <PhoneInTalk style={iconStyle} onMouseOver={() => {this.setState({message: 'talk to someone'})}}/>
+          <PhoneInTalk
+            style={iconStyle}
+            onMouseOver={() => {
+              this.setState({message: 'Interact'})
+            }}
+            onMouseLeave={() => {
+              this.setState({message: this.state.location.message})
+            }}              
+          />
         </Grid>
       </Grid>
     </Grid>
-  )
-  }
+  )}
 }
 
 export default Game
